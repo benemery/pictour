4a5f1a3e81e6873d8c0bafb4347b059a0f6e49b4
Ben Emery <ben.s.emery@gmail.com>
2015-01-24 11:26:16 +0000

    Force users to login before we can view your tours

--- photo_geoip/views.py
+++ photo_geoip/views.py
@@ -8,6 +8,7 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.files import File
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.generic import View
 
 from dropbox.client import DropboxClient
@@ -103,18 +104,13 @@ def process_user(uid):
         # Repeat only if there's more to do
         has_more = result['has_more']
 
-
-from django.shortcuts import render
-
 class YourTours(View):
     @classmethod
     def as_view(cls, **initkwargs):
         view = super(YourTours, cls).as_view(**initkwargs)
-        return (view)
+        return login_required(view, login_url='/#about')
 
     def get(self, request):
         user = request.user
-
         tours = user.tours.all().select_related('completed_steps')
-
         return render(request, 'your_tours.html', {'tours': tours})
