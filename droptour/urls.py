e09aa7228935f13d86042b3be8ff36becb2396d0
Ollie Bennett <ollie.bennett@locatable.com>
2015-01-24 02:12:55 +0000

    Configure URL for templates

--- droptour/urls.py
+++ droptour/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^', include('photo_geoip.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^', TemplateView.as_view(template_name="index.html")),
+
 )
